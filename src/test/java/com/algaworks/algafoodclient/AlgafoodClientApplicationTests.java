d82ff344c32f00252ae36d1d1be25b54f6dac1d1
mucheniski <mucheniski@gmail.com>
2021-03-17 08:27:36 -0300

    Projeto criado

---
+++ src/test/java/com/algaworks/algafoodclient/AlgafoodClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
